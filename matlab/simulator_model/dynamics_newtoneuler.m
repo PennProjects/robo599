75114b63b8e6128e97545ec9d09921cff4a1acaf
Jal Panchal <jlpnchl@gmail.com>
2021-04-27 15:46:46 -0400

    upadted jacobian calculations

--- matlab/simulator_model/dynamics_newtoneuler.m
+++ matlab/simulator_model/dynamics_newtoneuler.m
@@ -1,4 +1,4 @@
-%calculation of Dyna
+%calculation of Dynamics
 
 %assigning weight sto each limb section
 
@@ -23,79 +23,17 @@ total_limb_mass = (mass_rh_gm +mass_lh_gm + mass_rl_gm + mass_ll_gm);
 
 baby_body = baby_body_points(); 
 
-%getting joint positions
-for i = 0:5:180
-    
-    %calculating joint postion for given input angle
-    [~,r_hand] = calc_rh_pos(i);
-    [~,r_leg] = calc_rl_pos(i);
-    
-    [~,l_hand] = calc_lh_pos(0);
-    [~,l_leg] = calc_ll_pos(0);
+%%
+%testing Newton-euler torque calculations
 
-    
-    
-    
-    %calculating com_x = sum(x_i*mass_i)/sum(mass_i)
-    com_x = (r_hand(3,1)*mass_rh_arm_gm + r_hand(5,1)*mass_rh_forearm_gm +...
-            l_hand(3,1)*mass_lh_arm_gm + l_hand(5,1)*mass_lh_forearm_gm +...
-            r_leg(3,1)*mass_rl_thigh_gm + r_leg(5,1)*mass_rl_calf_gm +...
-            l_leg(3,1)*mass_ll_thigh_gm + l_leg(5,1)*mass_ll_calf_gm)/...
-            (total_limb_mass);
-            
-    %calculating com_y = sum(y_i*mass_i)/sum(mass_i)
-    com_y = (r_hand(3,2)*mass_rh_arm_gm + r_hand(5,2)*mass_rh_forearm_gm +...
-            l_hand(3,2)*mass_lh_arm_gm + l_hand(5,2)*mass_lh_forearm_gm +...
-            r_leg(3,2)*mass_rl_thigh_gm + r_leg(5,2)*mass_rl_calf_gm +...
-            l_leg(3,2)*mass_ll_thigh_gm + l_leg(5,2)*mass_ll_calf_gm)/...
-            (total_limb_mass);
-        
-    %calculating com_z = sum(z_i*mass_i)/sum(mass_i)    
-    com_z = (r_hand(3,3)*mass_rh_arm_gm + r_hand(5,3)*mass_rh_forearm_gm +...
-            l_hand(3,3)*mass_lh_arm_gm + l_hand(5,3)*mass_lh_forearm_gm +...
-            r_leg(3,3)*mass_rl_thigh_gm + r_leg(5,3)*mass_rl_calf_gm +...
-            l_leg(3,3)*mass_ll_thigh_gm + l_leg(5,3)*mass_ll_calf_gm)/...
-            (total_limb_mass);
-    
-        
-    %plotting
-    %plot 3D simulator model
-    subplot(1,2,1);
-    plot3(r_hand(:,1), r_hand(:,2), r_hand(:,3),'o-','LineWidth', 2,'color','r');
-    hold on
-    plot3(r_leg(:,1), r_leg(:,2), r_leg(:,3),'o-','LineWidth', 2,'color','b');
-    plot3(l_hand(:,1), l_hand(:,2), l_hand(:,3),'o-','LineWidth', 2,'color','r');
-    plot3(l_leg(:,1), l_leg(:,2), l_leg(:,3),'o-','LineWidth', 2,'color','b');
-    plot3(baby_body(:,1), baby_body(:,2), baby_body(:,3),'o-','LineWidth', 2,'color','black');
-    
-    hold off
-    grid on
-    view(0,70);
-    
-    xlabel('Xo', 'FontSize', 20, 'FontWeight', 'bold');
-    ylabel('Yo', 'FontSize', 20, 'FontWeight', 'bold');
-    zlabel('Zo', 'FontSize', 20, 'FontWeight', 'bold');
-    title('Angled View')
-    
-    xlim([-300,300])
-    ylim([-300,250])
-    zlim([-30,200])
-    
-    %plot COM
-    subplot(1,2,2);
-    plot3(com_x,com_y, com_z, 'o', 'color','r')
-    hold on
-    xlabel('Xo', 'FontSize', 20, 'FontWeight', 'bold');
-    ylabel('Yo', 'FontSize', 20, 'FontWeight', 'bold');
-    zlabel('Zo', 'FontSize', 20, 'FontWeight', 'bold');
-    title('COM X-Y plane')
-    xlim([-100,100])
-    ylim([-200,100])
-    view(0,90);
+%get jacobians for input angles
+i = 0; %in degrees
+[~,~, j_rh] = calc_rh_pos(i);
+[~,~, j_rl] = calc_rl_pos(i);
 
-    grid on
-    
-    drawnow
-    
-    
-end
+[~,~, j_lh] = calc_lh_pos(0);
+[~,~, j_ll] = calc_ll_pos(0);
+
+
+%%torque = J'*Force
+
