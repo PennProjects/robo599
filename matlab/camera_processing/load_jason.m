88deed89d980048787598c8f5c95df2bb4a8e6cc
Jal Panchal <jlpnchl@gmail.com>
2021-05-04 01:36:16 -0400

    loading json

--- matlab/camera_processing/load_jason.m
+++ matlab/camera_processing/load_jason.m
@@ -1,6 +1,3 @@
-command = 'pwd';
-[~,cmdout] = system(command)
-
 %% Generate Openpose Command
 %navigate to openpose
 command = "cd /Users/jalpanchal/git/openpose/";
@@ -36,9 +33,29 @@ for i = 1:numel(files)
     json_out_cmd = strcat(path_to_op,input_param,video_file,model_param,output_param_json,op_out_path)
 end
 
-    
-    %%
+%% Open json files
 
-pathg_to_video = "/Users/jalpanchal/drive/penn/robo599/simulator_media/"
-
-%--video /Users/jalpanchal/drivepenn/robo599/simulator_media/0422/lfthnd/cam_raw/gp_0422_lfthnd_2.MP4 --model_pose COCO --part_candidates --write_video /Users/jalpanchal/drive/penn/robo599/simulator_media/0422/lfthnd/openpose_out/gp_pose_0422_lfthnd_2.MP4
+file_name = '/Users/jalpanchal/drive/penn/robo599/simulator_media/0429/right_leg/openpose_out/gp_0429_rgtleg_1_trim_000000000037_keypoints.json'; 
+fid = fopen(file_name); 
+raw = fread(fid,inf); 
+str = char(raw');
+fclose(fid); 
+val = jsondecode(str);
+%%
+frame_num = str2num(file_name(end-18:end-15))
+%%
+body_points = {'x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14', 'x15', 'x16', 'x17'};
+joint_idx = zeros(18,1);
+joint_pos= zeros(18,3);
+frame_num = zeros(18,1);
+for i = 1:18
+    frame_num(i) = str2num(file_name(end-18:end-15));
+    joint_idx(i) = i-1;
+    joint_pos(i,:) = val.part_candidates.(body_points{i})(1:3)';
+end    
+%%
+data_headers  = {'frame_num', 'joint_idx', 'x', 'y', 'c'};
+a = cell2table(data_headers, VariableNames, data_headers)
+%%
+a = struct2table(val.part_candidates(1,1), 'AsArray',true)
+b = rows2vars(a)
