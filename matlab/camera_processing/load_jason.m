9b7ad445b5a91ea1172dbf97ad17f113f51624a0
Jal Panchal <jlpnchl@gmail.com>
2021-05-05 07:51:22 -0400

    first results camera to world joint plot

--- matlab/camera_processing/load_jason.m
+++ matlab/camera_processing/load_jason.m
@@ -67,14 +67,86 @@ r_ankle_y = pose_raw.y(pose_raw.joint_idx ==10);
 figure();
 subplot(2,1,1)
 plot(r_ankle_x);
+title("Ankle X position")
 
 subplot(2,1,2)
 plot(r_ankle_y);
+title("Ankle Y position")
+suptitle("Joint position in pixels")
 % for i =1:5:size(r_ankle_x,1)
 %     plot(r_ankle_x(i), r_ankle_y(i), 'o-')
 %     hold on
 % end
 
+%% Translate to World coordinates
+xy_pixel = table2array( pose_raw(:,3:4));
+
+xy_world = array2table(pointsToWorld(cameraParams, R, t, xy_pixel));
+
+pose_world = pose_raw;
+% pose_world.Properties.VariableNames = {'frame_num', 'joint_idx', 'x_cal', 'y_cal', 'c'};
+pose_world.x= xy_world.Var1;
+pose_world.y= xy_world.Var2;
+
 %%
-a = pose_raw(pose_raw.joint_idx ==10,:);
+%test plot of wrist
+r_ankle_x = pose_world.x(pose_world.joint_idx ==10);
+r_ankle_y = pose_world.y(pose_world.joint_idx ==10);
+
+figure();
+subplot(2,1,1)
+plot(r_ankle_x);
+title("Ankle X position")
+
+subplot(2,1,2)
+plot(r_ankle_y);
+title("Ankle Y position")
+suptitle("Joint position in World")
+
+
+%% Transpose all poisnt to joint 1 reference
+
+%% plot body joints
+body_points = pose_raw;
+
+total_frames = body_points.frame_num(end);
+for f = 0:total_frames
+    frame_points = body_points(body_points.frame_num ==f,:);
+    
+    %right hand = 1,2,3,4
+    rh_points = table2array([frame_points(frame_points.joint_idx==1,3:4);
+                 frame_points(frame_points.joint_idx==2,3:4);
+                 frame_points(frame_points.joint_idx==3,3:4);
+                 frame_points(frame_points.joint_idx==4,3:4)]);
+    %left hand = 1,5,6,7
+    lh_points = table2array([frame_points(frame_points.joint_idx==1,3:4);
+                 frame_points(frame_points.joint_idx==5,3:4);
+                 frame_points(frame_points.joint_idx==6,3:4);
+                 frame_points(frame_points.joint_idx==7,3:4)]);
+    %right leg = 1,8,9,10
+    rl_points = table2array([frame_points(frame_points.joint_idx==1,3:4);
+                 frame_points(frame_points.joint_idx==8,3:4);
+                 frame_points(frame_points.joint_idx==9,3:4);
+                 frame_points(frame_points.joint_idx==10,3:4)]);
+    %left leg  = 1,11,12,13
+    ll_points = table2array([frame_points(frame_points.joint_idx==1,3:4);
+                 frame_points(frame_points.joint_idx==11,3:4);
+                 frame_points(frame_points.joint_idx==12,3:4);
+                 frame_points(frame_points.joint_idx==13,3:4)]);
+%     rh_points = table2array(rh_points);
+%     lh_points = table2array(lh_points);
+    
+    plot(rh_points(:,1), rh_points(:,2), 'o-', 'LineWidth', 2,'color','r');
+    set(gca, 'YDir','reverse')
+    hold on
+    plot(lh_points(:,1), lh_points(:,2), 'o-','LineWidth', 2,'color','b');
+    plot(rl_points(:,1), rl_points(:,2), 'o-','LineWidth', 2,'color','r');
+    plot(ll_points(:,1), ll_points(:,2), 'o-','LineWidth', 2,'color','b');
+    hold off
+    grid on 
+%     xlim([-300,300])
+%     ylim([-100,350])
+    title("Body Joints Calibrated")
+    drawnow
+end    
 
