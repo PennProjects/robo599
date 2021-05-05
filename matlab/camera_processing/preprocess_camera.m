80c5e3bde0a035372a1c4c6934922cac06ac5832
Jal Panchal <jlpnchl@gmail.com>
2021-05-05 10:35:02 -0400

    preprocessing v1

--- matlab/camera_processing/preprocess_camera.m
+++ matlab/camera_processing/preprocess_camera.m
@@ -1,38 +1,8 @@
-%% Generate Openpose Command
-%navigate to openpose
-command = "cd /Users/jalpanchal/git/openpose/";
-system(command);
-
-path_to_op = "./build/examples/openpose/openpose.bin ";
-input_param = "--video ";
-model_param = " --model_pose COCO --part_candidates ";
-output_param_video ="--write_video ";
-output_param_json = "--write_json ";
-
 %experiment details
 path_to_media = '/Users/jalpanchal/drive/penn/robo599/simulator_media/';
 date = {'0429/'};
 limb = {'right_hand/', 'left_hand/','right_leg/', 'left_leg/'};
 device = {'camera_trim/', 'openpose_out/'};
-
-%gen
-limb_idx = 1;
-vid_in_path = strcat(path_to_media,date{1},limb{limb_idx},device{1});
-op_out_path = strcat(path_to_media,date{1},limb{limb_idx},device{2});
-files = dir(strcat(vid_in_path,'*.mp4'));
-for i = 1:numel(files)
-    video_file = strcat(vid_in_path,files(i).name);
-    out_file = strcat(op_out_path,"out_",files(i).name);
-    
-    fprintf(limb{limb_idx})
-    fprintf("Command for Experiment : %d",i);
-    %command for video out
-    video_out_cmd = strcat(path_to_op,input_param,video_file,model_param,output_param_video,out_file)  
-    
-    %%command for json out
-    json_out_cmd = strcat(path_to_op,input_param,video_file,model_param,output_param_json,op_out_path)
-end
-
 %% Open json files
 op_out_path = strcat(path_to_media,date{1},limb{3},device{2});
 files = dir(strcat(op_out_path,'*.json'));
@@ -80,6 +50,8 @@ suptitle("Joint position in pixels")
 % end
 
 %% Translate to World coordinates
+%load calibration data
+load("/Users/jalpanchal/drive/penn/robo599/simulator_media/0429/calibration/camera_calibration.mat");
 xy_pixel = table2array( pose_raw(:,3:4));
 
 xy_world = array2table(pointsToWorld(cameraParams, R, t, xy_pixel));
@@ -156,4 +128,18 @@ for f = 0:total_frames
     title("Body Joints Calibrated")
     drawnow
 end    
+
+%% plot time series
+r_ankle_x = pose_sim.x(pose_sim.joint_idx ==10);
+r_ankle_y = pose_sim.y(pose_sim.joint_idx ==10);
+
+figure();
+subplot(2,1,1)
+plot(r_ankle_x);
+title("Ankle X position")
+
+subplot(2,1,2)
+plot(r_ankle_y);
+title("Ankle Y position")
+suptitle("End-Effector position in Body Frame "+"Limb : Right Leg")
 
