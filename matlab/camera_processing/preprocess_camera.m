69adc2672acf1e8aabb9a30fca2b6b8b48ef47e5
Jal Panchal <jlpnchl@gmail.com>
2021-05-08 08:51:03 -0400

    all trial upload

--- matlab/camera_processing/preprocess_camera.m
+++ matlab/camera_processing/preprocess_camera.m
@@ -5,35 +5,41 @@ limb = {'right_hand/', 'left_hand/','right_leg/', 'left_leg/'};
 device = {'camera_trim/', 'openpose_out/'};
 %% Open json files
 op_out_path = strcat(path_to_media,date{1},limb{3},device{2});
-files = dir(strcat(op_out_path,'*.json'));
+folder_name = dir(strcat(op_out_path,'gp_*'));
 
 body_points = {'x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14', 'x15', 'x16', 'x17'};
-pose_raw = array2table(zeros(1,5));
-for i = 1:numel(files)
-   file_name = strcat(op_out_path,files(i).name);
-   
-   %open and copy file content
-   fid = fopen(file_name); 
-   raw = fread(fid,inf);
-   str = char(raw');
-   fclose(fid);
-   json_data = jsondecode(str);
-   
-   for j = 1:18
-       frame_num = str2num(file_name(end-18:end-15));
-       joint_idx = j-1;
-       joint_pos = json_data.part_candidates.(body_points{j})(end-2:end)';
-       temp_ = array2table([frame_num, joint_idx, joint_pos]);
-       pose_raw = [pose_raw;temp_];
-   end   
+pose_raw = array2table(zeros(1,6));
+
+for k = 1:numel(folder_name)
+    trial_name = str2num(folder_name(k).name(end-5:end-5));
+    files = dir(strcat(op_out_path,folder_name(k).name,'/','*.json'));
+    for i = 1:numel(files)
+        file_name = strcat(op_out_path,folder_name(k).name,'/',files(i).name);
+        
+        %open and copy file content
+        fid = fopen(file_name);
+        raw = fread(fid,inf);
+        str = char(raw');
+        fclose(fid);
+        json_data = jsondecode(str);
+        
+        for j = 1:18
+            trial_num = trial_name;
+            frame_num = str2num(file_name(end-18:end-15));
+            joint_idx = j-1;
+            joint_pos = json_data.part_candidates.(body_points{j})(end-2:end)';
+            temp_ = cell2table([{trial_num,frame_num, joint_idx} ,num2cell(joint_pos)]);
+            pose_raw = [pose_raw;temp_];
+        end
+    end
 end
-pose_raw.Properties.VariableNames = {'frame_num', 'joint_idx', 'x', 'y', 'c'};
+pose_raw.Properties.VariableNames = {'trial_num','frame_num', 'joint_idx', 'x', 'y', 'c'};
 pose_raw(1,:) = [];
 
 %%
 %test plot of wrist
-r_ankle_x = pose_raw.x(pose_raw.joint_idx ==10);
-r_ankle_y = pose_raw.y(pose_raw.joint_idx ==10);
+r_ankle_x = pose_raw.x(pose_raw.joint_idx ==10 & pose_raw.trial_num == 2);
+r_ankle_y = pose_raw.y(pose_raw.joint_idx ==10 & pose_raw.trial_num == 2);
 
 figure();
 subplot(2,1,1)
