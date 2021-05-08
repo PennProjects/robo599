b0d841f7fded794574b16fb9060785cb0b7aaa19
Jal Panchal <jlpnchl@gmail.com>
2021-05-08 08:56:59 -0400

    update cmd file

--- matlab/camera_processing/openpose_cmd.m
+++ matlab/camera_processing/openpose_cmd.m
@@ -38,31 +38,3 @@ for i = 1:numel(files)
     both_out_cmd = strcat(path_to_op,input_param,video_file,model_param,output_param_video,out_file,output_param_json,out_folder)
 end
 
-%% Open json files
-op_out_path = strcat(path_to_media,date{1},limb{3},device{2});
-files = dir(strcat(op_out_path,'*.json'));
-
-body_points = {'x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14', 'x15', 'x16', 'x17'};
-pose_raw = array2table(zeros(1,5));
-for i = 1:numel(files)
-   file_name = strcat(op_out_path,files(i).name);
-   
-   %open and copy file content
-   fid = fopen(file_name); 
-   raw = fread(fid,inf);
-   str = char(raw');
-   fclose(fid);
-   json_data = jsondecode(str);
-   
-   for j = 1:18
-       frame_num = str2num(file_name(end-18:end-15));
-       joint_idx = j-1;
-       joint_pos = json_data.part_candidates.(body_points{j})(end-2:end)';
-       temp_ = array2table([frame_num, joint_idx, joint_pos]);
-       pose_raw = [pose_raw;temp_];
-   end   
-end
-pose_raw.Properties.VariableNames = {'frame_num', 'joint_idx', 'x', 'y', 'c'};
-pose_raw(1,:) = [];
-
-%%
